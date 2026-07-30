96421035cef6dc5366bd4293fdbee91ecc47c84a
Michael Odom <mjodom@open5gs.odom.home>
2026-07-30 12:34:14 -0500

    Personalization updates

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,6 +6,19 @@ return {
       options = {
         theme = 'tokyonight',
         icons_enabled = true,
+      },
+      sections = {
+        -- Keep standard default modules for the left and center sections
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        
+        -- Customized right sections
+        lualine_y = {
+          { "%2p%%" } -- Replaces percentage with Current Line / Total Lines (e.g., 42/150)
+        },
+        lualine_z = {'%4l(%L):%-2c'} -- Keeps the standard "Line:Column" display
       }
     })
   end
